0cb272541b3728cd8ee1f6cdf5480a05be8bff91
Sadeq Sheikhi <sadeghshaikhi77@gmail.com>
2020-12-22 11:17:41 +0330

    secondary-nav Implemented UI

--- src/js/topNav.js
+++ src/js/topNav.js
@@ -1,7 +1,6 @@
 (function() {
 const dropDowns = document.querySelectorAll('.dropdown-holder');
 
-
 dropDowns.forEach(function(node) {
     node.addEventListener('mouseover', function() {
         node.querySelector('.main-dropdown').style="display:initial";
@@ -10,11 +9,17 @@ dropDowns.forEach(function(node) {
     node.addEventListener('mouseout', function() {
         node.querySelector('.main-dropdown').style="display:none";
     })
-
-    
 })
 
 
+let hamburgerButton = document.querySelector("#menu-toggle")
+let secondaryMenu = document.querySelector("#secondary-menu")
+hamburgerButton.addEventListener('click', function() {
+    console.log('hiiii')
+    secondaryMenu.classList.toggle('hidden')
+    secondaryMenu.classList.toggle("md:hidden")
+    console.log('hey')
+})
 
 
 
