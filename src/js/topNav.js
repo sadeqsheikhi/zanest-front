5de4f2985133f17f893f853b2dde462c8f8ecd55
sadeqsheikhi <sadeghshaikhi77@gmail.com>
2021-03-26 23:02:36 +0430

    Changing Whole Logic To Use Singleton For Database

--- src/js/topNav.js
+++ src/js/topNav.js
@@ -1,4 +1,26 @@
-(function() {
+let secondTexts = document.querySelectorAll('.second-nav-text')
+const menuToggle = document.querySelector('#menu-toggle')
+const secondaryMenu = document.querySelector('#secondary-menu')
+const secondaryMenuEls = document.querySelectorAll('.side-nav-li')
+menuToggle.addEventListener('click', e=> {
+        if (secondaryMenu.classList.contains('w-56')) {
+            secondaryMenu.classList.remove('w-56')
+            secondaryMenu.classList.add('w-16')
+            secondaryMenuEls.forEach(node => {
+                node.classList.add('closed')
+            })
+        } else {
+            secondaryMenu.classList.remove('w-16')
+            secondaryMenu.classList.add('w-56')
+            secondaryMenuEls.forEach(node => {
+                node.classList.remove('closed')
+            })
+        }
+})
+
+
+
+
 const dropDowns = document.querySelectorAll('.dropdown-holder');
 
 dropDowns.forEach(function(node) {
@@ -12,13 +34,3 @@ dropDowns.forEach(function(node) {
 })
 
 
-let hamburgerButton = document.querySelector("#menu-toggle")
-let secondaryMenu = document.querySelector("#secondary-menu")
-hamburgerButton.addEventListener('click', function() {
-    console.log('hiiii')
-    secondaryMenu.classList.toggle('hidden')
-    secondaryMenu.classList.toggle("md:hidden")
-    console.log('hey')
-})
-
-})()
